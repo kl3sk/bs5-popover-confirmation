44696f70b2d8b07bf80e9bac24417e3c3195dbbe
Grégoire <gregoire.houssais@bellvision.fr>
2023-09-19 12:23:48 +0200

    App: try to fix build

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,37 @@
-const path = require('path')
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
-export default {
-    root: path.resolve(__dirname, 'src'),
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
     resolve: {
         alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
         }
     },
     build: {
         outDir: '../dist',
-        manifest: true,
-        minify: true,
-        reportCompressedSize: true,
         lib: {
-            entry: path.resolve(__dirname, "src/js/main.js"),
-            fileName: "main",
-            formats: ["es", "cjs"],
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/js/main.js'),
+            name: 'Bs popover confirmation',
+            // the proper extensions will be added
+            fileName: 'bs-popover-confirmation',
         },
         rollupOptions: {
-            external: [],
-        }
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                external: ['bootstrap'],
+                globals: {
+                    bootstrap: 'bootstrap',
+                },
+            },
+        },
     },
     server: {
         port: 8080,
         hot: true
     }
-}
+})
